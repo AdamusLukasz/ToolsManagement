ff45877799be9b3fe92ba52afcbe4495cee752f9
PyLukasz <as.lukasz@gmail.com>
2022-11-09 10:08:20 +0100

    Add new exception for wrong value.

---
+++ ToolsManagement/Exceptions/WrongValueException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ToolsManagement.Exceptions
+{
+    public class WrongValueException : Exception
+    {
+        public WrongValueException(string message) : base(message)
+        {
+
+        }
+    }
+}
