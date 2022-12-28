18327108f4a1961479df5ca97a4226a8b49abb7f
PyLukasz <as.lukasz@gmail.com>
2022-12-28 23:45:55 +0100

    Added controller to user registeration.

---
+++ ToolsManagement/Controllers/AccountController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToolsManagement.Controllers
+{
+    [Route("api/account")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        [HttpPost("register")]
+        public ActionResult RegisterUser([FromBody] RegisterUserDto dto)
+        {
+
+        }
+    }
+}
