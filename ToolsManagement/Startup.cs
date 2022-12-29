def2c883acfb64760ae90f83c223ea9941e0ef48
PyLukasz <as.lukasz@gmail.com>
2022-12-29 13:07:54 +0100

    Configuration jwt auth

--- ToolsManagement/Startup.cs
+++ ToolsManagement/Startup.cs
@@ -24,6 +24,8 @@ using FluentValidation;
 using ToolsManagement.Models;
 using ToolsManagement.Models.Validators;
 using FluentValidation.AspNetCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 namespace ToolsManagement
 {
@@ -39,7 +41,26 @@ namespace ToolsManagement
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            var authenticationSettings = new AuthenticationSettings();
 
+            Configuration.GetSection("Authentication").Bind(authenticationSettings);
+
+            services.AddAuthentication(option =>
+            {
+                option.DefaultAuthenticateScheme = "Bearer";
+                option.DefaultScheme = "Bearer";
+                option.DefaultChallengeScheme = "Bearer";
+            }).AddJwtBearer(cfg =>
+            {
+                cfg.RequireHttpsMetadata = false;
+                cfg.SaveToken = true;
+                cfg.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidIssuer = authenticationSettings.JwtIssuer,
+                    ValidAudience = authenticationSettings.JwtIssuer,
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(authenticationSettings.JwtKey))
+                };
+            });
             services.AddControllers().AddFluentValidation();
             services.AddMvc()
                     .AddNewtonsoftJson(options =>
@@ -71,6 +92,7 @@ namespace ToolsManagement
                 app.UseDeveloperExceptionPage();
                 
             }
+            app.UseAuthentication();
             app.UseSwagger();
             app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "ToolsManagement v1"));
 
