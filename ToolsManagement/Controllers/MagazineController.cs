15deb6f020592be095ee35483a485b3480e9c6de
PyLukasz <as.lukasz@gmail.com>
2022-12-31 17:20:19 +0100

    added authorisation just for admin to update the number of drills in magazine.

--- ToolsManagement/Controllers/MagazineController.cs
+++ ToolsManagement/Controllers/MagazineController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -42,7 +43,7 @@ namespace ToolsManagement.Controllers
             return Ok();
 
         }
-
+        [Authorize(Roles = "Admin")]
         [HttpPut("updatemagazine/{drillId}")]
         public ActionResult UpdateQuantityInMagazine([FromRoute] int drillId, [FromBody] DrillMagazineQuantityUpdateDto dto)
         {
